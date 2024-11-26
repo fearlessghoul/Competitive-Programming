d5f0bbf5a425bc141b40636b218f82cf24f939a0
Ken Kaneki <fearlessghoul8699@gmail.com>
2024-11-26 16:00:21 +0530

    string concat

--- LeetcodeQuestions/stringofallconcats.cpp
+++ LeetcodeQuestions/stringofallconcats.cpp
@@ -1,16 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long compute_hash(string const &s){
-    cout<<s.length()<<endl;
-    const int p = 2;
-    long long hash_value = 0;
-    long long p_pow = 1;
-    for (char c : s){
-        hash_value = (hash_value + (c - 'a' + 1) * p_pow);
-        p_pow = (p_pow * p);
+class Solution {
+public:
+    vector<int> findSubstring(string s, const vector<string>& words) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        vector<int> result;
+        unordered_map<string, unsigned int> map;
+        unsigned int length = words[0].size();
+        map.clear();
+        for (const string& word : words) map[word]++;
+        for (unsigned int offset = 0; offset < length; ++offset) {
+            unsigned int size = 0;
+            unordered_map<string, unsigned int> seen;
+            for (unsigned int i = offset; i + length <= s.size(); i += length) {
+                string sub = s.substr(i, length);
+                auto itr = map.find(sub);
+                if (itr == map.end()) {
+                    seen.clear();
+                    size = 0;
+                    continue;
+                }
+
+                ++seen[sub];
+                ++size;
+                while (seen[sub] > itr->second) {
+                    std::string first = s.substr(i - (size - 1) * length, length);
+                    --seen[first];
+                    --size;
+                }
+                
+                if (size == words.size())
+                    result.push_back(i - (size - 1) * length);
+            }
+        }
+
+        return result;
     }
-    return hash_value/32;
-}
+};
 int main(){
-    cout << compute_hash("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz") << endl;
+    Solution a;
+    vector<int> res=a.findSubstring("barfoothefoobarman",{"foo","bar"});
+    for(int a:res){
+        cout<<a<<" ";
+    }
+    cout<<endl;
 }
